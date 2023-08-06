4c18f69c7ae7bd975e3c4afafc035e7b5ba6b6ef
Nansess <75940580+Nansess@users.noreply.github.com>
2023-08-06 09:49:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const tmi = require('tmi.js');
 require('dotenv').config();
 
-// List of default blocked words
 const defaultBlockedWords = ['badword1', 'badword2', 'fword', 'inappropriate'];
 
-// Initialize the list of blocked words with the default ones
 let blockedWords = [...defaultBlockedWords];
 
 const twitchBotOptions = {
@@ -25,26 +23,21 @@ const twitchBotOptions = {
 const twitchClient = new tmi.client(twitchBotOptions);
 twitchClient.connect();
 
-// Command Cooldown
 const commandCooldowns = new Set();
 const COMMAND_COOLDOWN_TIME = 5000; // 5 seconds
 
-// Add a global catch block for unhandled promise rejections
 process.on('unhandledRejection', (reason, promise) => {
   console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
 
-// Event handler for incoming messages
 twitchClient.on('message', (channel, userstate, message, self) => {
-  if (self) return; // Ignore messages from the bot itself
+  if (self) return; //
 
   console.log(`Received message: ${message}`);
 
-  // Moderation logic - Check for blocked words
   const lowercaseMessage = message.toLowerCase();
   for (const blockedWord of blockedWords) {
     if (lowercaseMessage.includes(blockedWord)) {
-      // If the message contains a blocked word, delete the message and timeout the user for 10 seconds
       twitchClient.deletemessage(channel, userstate.id)
         .catch((err) => {
           console.error('Error deleting message:', err);
@@ -56,7 +49,7 @@ twitchClient.on('message', (channel, userstate, message, self) => {
         });
 
       console.log(`Blocked message from ${userstate.username}: ${message}`);
-      return; // Exit the loop and don't check for other blocked words
+      return; 
     }
   }
 
@@ -67,7 +60,6 @@ twitchClient.on('message', (channel, userstate, message, self) => {
 
     console.log(`Received command: ${command} from ${userstate.username}`);
 
-    // Custom command to add a word to the blocked list (for administrators/trusted users)
     if (userstate.mod && command === 'addblockedword') {
       const customWord = args[0];
       if (customWord) {
@@ -81,21 +73,18 @@ twitchClient.on('message', (channel, userstate, message, self) => {
       return;
     }
 
-    // Check if command is on cooldown
     if (commandCooldowns.has(command)) {
       twitchClient.say(channel, `@${userstate.username}, command "${command}" is on cooldown.`);
       console.log(`Command "${command}" is on cooldown for ${userstate.username}.`);
       return;
     }
 
-    // Process custom commands
     switch (command) {
       case 'hello':
         twitchClient.say(channel, `Hello, @${userstate.username}!`);
         console.log(`Sent greeting to ${userstate.username}.`);
         break;
       case 'clear':
-        // Check if the user is a moderator before allowing the command
         if (userstate.mod) {
           twitchClient.clear(channel);
           twitchClient.say(channel, 'Chat cleared by a moderator.');
@@ -141,7 +130,6 @@ twitchClient.on('message', (channel, userstate, message, self) => {
         console.log(`Bot uptime: ${hours}h ${minutes}m ${seconds}s.`);
         break;
       case 'quote':
-        // Custom command to display a random quote
         const quotes = [
           "Be yourself; everyone else is already taken. - Oscar Wilde",
           "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
@@ -154,17 +142,14 @@ twitchClient.on('message', (channel, userstate, message, self) => {
         console.log(`Displayed a random quote.`);
         break;
       case 'followage':
-        // Custom command to show how long the user has been following the channel
         // (Note: This requires using the Twitch API or a third-party service)
         twitchClient.say(channel, `@${userstate.username}, you have been following for a while!`);
         console.log(`${userstate.username} asked for their followage.`);
         break;
-      // Add more custom commands here
       default:
         break;
     }
 
-    // Add command to cooldown
     commandCooldowns.add(command);
     setTimeout(() => {
       commandCooldowns.delete(command);
@@ -173,12 +158,10 @@ twitchClient.on('message', (channel, userstate, message, self) => {
   }
 });
 
-// Event handler for the bot connecting to Twitch
 twitchClient.on('connected', (address, port) => {
   console.log(`Connected to Twitch at ${address}:${port}`);
 });
 
-// Event handler for errors
 twitchClient.on('error', (err) => {
-  console.error('Twitch bot error:', err);
+  console.error('error:', err);
 });
